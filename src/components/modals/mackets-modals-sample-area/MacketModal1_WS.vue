72bcc50b53fb14d979ffd1e58c1da93c754757dc
ssandro25 <andro.misakiani@gmail.com>
2024-03-28 20:50:47 +0400

    fix: tax card in sample area

--- src/components/modals/mackets-modals-sample-area/MacketModal1_WS.vue
+++ src/components/modals/mackets-modals-sample-area/MacketModal1_WS.vue
@@ -249,18 +249,12 @@ export default {
 
     methods: {
         add() {
-            // this.arr = this.getWorkSpace
-            //     .find(item => item.id === parseInt(this.getWorkSpaceID)).forestryWS
-            //     .find(item => item.id === parseInt(this.getForestryWS_ID)).quarterWS
-            //     .find(item => item.id === parseInt(this.getQuarterWS_ID)).literWS
-            //     .find(item => item.id === parseInt(this.$route.params.id))
-
             this.arr = this.getWorkSpace
                 .find(item => item.id === parseInt(this.params.workSpaceID)).forestryWS
                 .find(item => item.id === parseInt(this.params.forestryWS_ID)).quarterWS
                 .find(item => item.id === parseInt(this.params.quarterWS_ID)).literWS
                 .find(item => item.id === parseInt(this.params.literWS_ID)).taxCardArr
-                .find(item => item.id === parseInt(this.params.macketID))
+                .find(item => item.id === parseInt(this.$route.params.id))
 
             console.log(this.arr)
 
@@ -296,7 +290,7 @@ export default {
             .find(item => item.id === parseInt(this.params.forestryWS_ID)).quarterWS
             .find(item => item.id === parseInt(this.params.quarterWS_ID)).literWS
             .find(item => item.id === parseInt(this.params.literWS_ID)).taxCardArr
-            .find(item => item.id === parseInt(this.params.macketID))
+            .find(item => item.id === parseInt(this.$route.params.id))
 
         if (!this.arr.mackets || !this.arr.mackets[0]) {
             this.arr.mackets = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
@@ -307,7 +301,7 @@ export default {
             .find(item => item.id === parseInt(this.params.forestryWS_ID)).quarterWS
             .find(item => item.id === parseInt(this.params.quarterWS_ID)).literWS
             .find(item => item.id === parseInt(this.params.literWS_ID)).taxCardArr
-            .find(item => item.id === parseInt(this.params.macketID)).mackets[0]
+            .find(item => item.id === parseInt(this.$route.params.id)).mackets[0]
 
         if (!(currentArr && currentArr[0] && (
             currentArr[0].soil_category !== '' ||
